9630fc806e8e490aea89899aa8d3ebad6be9f23d
Phillip Pargmann <pargmann92@gmail.com>
2020-03-05 18:21:38 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,102 +1,125 @@
-import React from 'react';
+import React from "react";
 
-import axios, { params } from './axios';
-import { InputGroupAddon, InputGroup, Input, Button } from 'reactstrap';
+import axios, { params } from "./axios";
+import { InputGroupAddon, InputGroup, Input, Button } from "reactstrap";
 
-import ItemList from './components/item-list/item-list.component';
-import ItemCard from './components/item-card/item-card.component';
+import ItemList from "./components/item-list/item-list.component";
+import ItemCard from "./components/item-card/item-card.component";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
-	state = {
-		hits: [],
-		searchInput: '',
-		item: null,
-		itemList: []
-	};
+  state = {
+    hits: [
+      {
+        fields: {
+          item_id: "513fceb675b8dbbc210026a5",
+          item_name: "White Bread - 1 slice, large",
+          brand_name: "USDA",
+          nf_serving_size_qty: 1,
+          nf_serving_size_unit: "serving"
+        }
+      }
+    ],
+    searchInput: "",
+    item: null,
+    itemList: [],
+    isOpen: false
+  };
 
-	componentDidMount() {
-		this.searchItems();
-	}
+  componentDidMount() {
+    this.searchItems();
+  }
 
-	searchItems = async (searchString) => {
-		if (!searchString) {
-			return;
-		}
-		await axios
-			.get('/search/' + searchString, {
-				params
-			})
-			.then((response) => {
-				console.log(response);
-				this.setState({ hits: [ ...response.data.hits ] });
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+  searchItems = async searchString => {
+    if (!searchString) {
+      return;
+    }
+    await axios
+      .get("/search/" + searchString, {
+        params
+      })
+      .then(response => {
+        console.log(response);
+        this.setState({ hits: [...response.data.hits] });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-	getItem = async (id) => {
-		await axios
-			.get('/item?id=' + id, {
-				params
-			})
-			.then((res) => {
-				console.log(res);
-				this.setState({
-					item: res.data
-				});
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
+  getItem = async id => {
+    await axios
+      .get("/item?id=" + id, {
+        params
+      })
+      .then(res => {
+        console.log(res);
+        this.setState({
+          item: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-	addToList = (item) => {
-		console.log(item);
-		this.setState({
-			itemList: [ ...this.state.itemList, item ]
-		});
-	};
+  addToList = item => {
+    console.log(item);
+    this.setState({
+      itemList: [...this.state.itemList, item]
+    });
+  };
 
-	render() {
-		return (
-			<div className="App scrollable">
-				<h1>Food Diary</h1>
-				<div className="grid ">
-					<div className="grid-column ">
-						<InputGroup className="input-group">
-							<Input
-								placeholder="Search for food"
-								onChange={(val) => this.setState({ searchInput: val.target.value })}
-							/>
-							<InputGroupAddon addonType="append">
-								<Button onClick={() => this.searchItems(this.state.searchInput)} color="primary">
-									To the Right!
-								</Button>
-							</InputGroupAddon>
-						</InputGroup>
+  handleClick = () => {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }));
+  };
 
-						<ItemList clickEvent={this.getItem} items={this.state.hits} />
-					</div>
-					<div className="grid-column">
-						{this.state.item !== null ? (
-							<ItemCard clickEvent={this.addToList} item={this.state.item} />
-						) : null}
+  render() {
+    return (
+      <div className="App scrollable">
+        <h1>Food Diary</h1>
 
-						{this.state.itemList.length > 0 ? (
-							this.state.itemList.map((item) => {
-								return <h4>{item.item_name}</h4>;
-							})
-						) : null}
-					</div>
-				</div>
-			</div>
-		);
-	}
+        <div className="grid ">
+          <div className="grid-column ">
+            <InputGroup className="input-group">
+              <Input
+                placeholder="Search for food"
+                onChange={val =>
+                  this.setState({ searchInput: val.target.value })
+                }
+              />
+              <InputGroupAddon addonType="append">
+                <Button
+                  onClick={() => this.searchItems(this.state.searchInput)}
+                  color="primary"
+                >
+                  To the Right!
+                </Button>
+              </InputGroupAddon>
+            </InputGroup>
+
+            <ItemList clickEvent={this.getItem} items={this.state.hits} />
+          </div>
+          <div className="grid-column">
+            {this.state.item !== null ? (
+              <ItemCard clickEvent={this.addToList} item={this.state.item} />
+            ) : null}
+
+            {this.state.itemList.length > 0
+              ? this.state.itemList.map(item => {
+                  return <h4>{item.item_name}</h4>;
+                })
+              : null}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/item/item.component.jsx
+++ src/components/item/item.component.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import { Toast, ToastBody, ToastHeader } from 'reactstrap';
+import { Toast, ToastBody, ToastHeader, Button } from "reactstrap";
 
-import './item.styles.scss';
+import "./item.styles.scss";
 
 const Item = ({ item, clickEvent }) => (
-	<div className="item" onClick={() => clickEvent(item.fields.item_id)}>
-		<Toast className="toast">
-			<ToastHeader className="toast-header">{item.fields.item_name}</ToastHeader>
-			<ToastBody className="toast-body">
-				<span className="toast-text">brand name: {item.fields.brand_name}</span>
-				<span className="toast-text">quantity: {item.fields.nf_serving_size_qty}</span>
-				<span className="toast-text">unit: {item.fields.nf_serving_size_unit}</span>
-			</ToastBody>
-		</Toast>
-	</div>
+  <div className="item" onClick={() => clickEvent(item.fields.item_id)}>
+    <Toast className="toast">
+      <ToastHeader className="toast-header">
+        {item.fields.item_name}
+      </ToastHeader>
+      <ToastBody className="toast-body">
+        <span className="toast-text">brand name: {item.fields.brand_name}</span>
+        <span className="toast-text">
+          quantity: {item.fields.nf_serving_size_qty}
+        </span>
+        <span className="toast-text">
+          unit: {item.fields.nf_serving_size_unit}
+        </span>
+
+        <Button style={{ zIndex: -1 }} id="pop" type="button">
+          Details
+        </Button>
+      </ToastBody>
+    </Toast>
+  </div>
 );
 
 export default Item;
